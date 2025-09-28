6b8c5d3b81596a5dd6ef70a5d29868a1f8e1143c
Mikey2001 <103912714+Mikey2001@users.noreply.github.com>
2025-09-28 22:42:36 +0300

    Create Lab2_1.c

---
+++ src/Lab2_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int sum_to_n(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+int main(void) {
+    int n;
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("Error: n must be >= 1.\n");
+    } else {
+        int result = sum_to_n(n);
+        printf("Sum of numbers from 1 to %d is %d\n", n, result);
+    }
+
+    return 0;
+}
