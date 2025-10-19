03baa4b24bd8d29db5776c859242b12671ab7c55
Mikey2001 <103912714+Mikey2001@users.noreply.github.com>
2025-10-19 18:33:24 +0300

    Create lab3_task2.c

---
+++ src/lab3_task2.c
@@ -0,0 +1,41 @@
+/*
+ * Lab 3, Task 2
+ * Student Name: Gamage Hirn Mitchell Perera
+ * Student ID: 231ADB188
+ *
+ * Practice using pointers as function parameters.
+ * Implement:
+ *   - swap (exchange values of two ints)
+ *   - modify_value (multiply given int by 2)
+ */
+
+#include <stdio.h>
+
+// Function prototypes
+void swap(int *x, int *y);
+void modify_value(int *x);
+
+int main(void) {
+    int a = 3, b = 7;
+
+    printf("Before swap: a=%d, b=%d\n", a, b);
+    swap(&a, &b);
+    printf("After swap: a=%d, b=%d\n", a, b);
+
+    modify_value(&a);
+    printf("After modify_value: a=%d\n", a);
+
+    return 0;
+}
+
+// Swap two integers
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// Multiply value by 2 (modify via pointer)
+void modify_value(int *x) {
+    *x = *x * 2;
+}
